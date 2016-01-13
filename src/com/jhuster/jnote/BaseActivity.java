88422bbe00fa004a45340c10e2f0a0d51e9a81c6
ticktick <lujun.hust@gmail.com>
2016-01-13 15:05:38 +0800

    修改代码格式

--- src/com/jhuster/jnote/BaseActivity.java
+++ src/com/jhuster/jnote/BaseActivity.java
@@ -1,3 +1,14 @@
+/*
+ *  Copyright (C) 2015, Jhuster, All Rights Reserved
+ *
+ *  Author:  Jhuster(lujun.hust@gmail.com)
+ *  
+ *  https://github.com/Jhuster/JNote
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ */
 package com.jhuster.jnote;
 
 import android.app.Activity;
